6cdf4fbebe0889f95749a78b522762f6b348ca68
Luca Mantani <luca.mantani@gmail.com>
2024-02-06 22:40:48 +0000

    Merged unified_models

--- colibri/models/wmin/wmin/model.py
+++ colibri/models/wmin/wmin/model.py
@@ -19,6 +19,19 @@ log = logging.getLogger(__name__)
 
 
 def pdf_model(wmin_settings):
+    """
+    Weight minimization grid is in the evolution basis.
+    The following parametrization is used:
+
+    f_{j,wm} = f_j + sum_i(w_i * (f_i - f_j))
+
+    this has the advantage of automatically satisfying the sum rules.
+
+    Notes:
+        - the central replica of the wminpdfset is always included in the
+          wmin parametrization
+    """
+
     return WMinPDF(PDF(wmin_settings["wminpdfset"]), wmin_settings["n_basis"])
 
 
@@ -45,8 +58,21 @@ class WMinPDF(PDFModel):
         return [f"w_{i+1}" for i in range(self.n_basis)]
 
     def grid_values_func(self, interpolation_grid):
-        """This function should produce a grid values function, which takes
+        """
+        This function should produce a grid values function, which takes
         in the model parameters, and produces the PDF values on the grid xgrid.
+
+        Weight minimization grid is in the evolution basis.
+        The following parametrization is used:
+
+        f_{j,wm} = f_j + sum_i(w_i * (f_i - f_j))
+
+        this has the advantage of automatically satisfying the sum rules.
+
+        Notes:
+            - the central replica of the wminpdfset is always included in the
+                wmin parametrization
+
         """
 
         input_grid = jnp.array(
@@ -56,12 +82,34 @@ class WMinPDF(PDFModel):
                 interpolation_grid,
                 [1.65],
             ).squeeze(-1)
-        )[: self.n_basis + 1, :, :]
+        )
+
+        if self.n_basis + 1 > input_grid.shape[0]:
+            raise ValueError(
+                "The number of basis functions is larger than the number of replicas in the wminpdfset."
+            )
+
+        # reduce INPUT_GRID to only keep n_replicas_wmin PDF replicas
+        wmin_basis_idx = jnp.arange(1, self.n_basis + 1)
+
+        # == generate weight minimization grid so that sum rules are automatically fulfilled == #
+        # pick central wmin replica as central replica from PDF set
+        wmin_central_replica = 0
+
+        # build wmin input grid so that sum rules are automatically fulfilled
+        wmin_input_grid = (
+            input_grid[wmin_basis_idx, :, :]
+            - input_grid[jnp.newaxis, wmin_central_replica]
+        )
+
+        wmin_input_grid = jnp.vstack(
+            (input_grid[jnp.newaxis, wmin_central_replica], wmin_input_grid)
+        )
 
         @jax.jit
         def wmin_param(weights):
             weights = jnp.concatenate((jnp.array([1.0]), jnp.array(weights)))
-            pdf = jnp.einsum("i,ijk", weights, input_grid)
+            pdf = jnp.einsum("i,ijk", weights, wmin_input_grid)
             return pdf
 
         return wmin_param
