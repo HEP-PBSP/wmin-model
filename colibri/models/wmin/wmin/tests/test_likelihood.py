4fe9a2f818d61c0d056dd3dc533b2f97ec774750
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-05-21 10:42:30 +0100

    use reduced datasets for likelihood speed test

--- colibri/models/wmin/wmin/tests/test_likelihood.py
+++ colibri/models/wmin/wmin/tests/test_likelihood.py
@@ -2,18 +2,20 @@
 Module for testing that the time per evaluation of the likelihood is below a certain threshold.
 """
 
-import jax
 import time
+
+import jax
 import pytest
 from colibri.api import API as colibriAPI
 from colibri.bayes_prior import bayesian_prior
 from colibri.tests.conftest import (
     T0_PDFSET,
-    TEST_FULL_DIS_DATASET,
-    TEST_FULL_POS_DATASET,
-    TEST_FULL_HAD_DATASET,
-    TEST_FULL_GLOBAL_DATASET,
+    TEST_DATASETS,
+    TEST_DATASETS_DIS_HAD,
+    TEST_DATASETS_HAD,
+    TEST_POS_DATASET,
 )
+from numpy.testing import assert_almost_equal
 from wmin.api import API as wminAPI
 from wmin.tests.wmin_conftest import (
     TEST_PRIOR_SETTINGS_WMIN,
@@ -22,12 +24,28 @@ from wmin.tests.wmin_conftest import (
 )
 
 N_LOOP_ITERATIONS = 100
-THRESHOLD_TIME_DIS = 1e-1
-THRESHOLD_TIME_DIS_POS = 3e-1
+THRESHOLD_TIME_DIS = 2.5e-3
+THRESHOLD_TIME_DIS_POS = 2.5e-3
 THRESHOLD_TIME_HAD = 3e0
 THRESHOLD_TIME_HAD_POS = 3e0
 THRESHOLD_TIME_GLOBAL = 3e0
 THRESHOLD_TIME_GLOBAL_POS = 3e0
+
+RNG_KEY = 0
+
+
+def prior_samples(prior, wmin_model_settings):
+    # Sample params from the prior
+    prior_samples = []
+    rng = jax.random.PRNGKey(RNG_KEY)
+    hypercube_samples = jax.random.uniform(
+        rng, shape=(N_LOOP_ITERATIONS, wmin_model_settings["wmin_settings"]["n_basis"])
+    )
+
+    for i in range(N_LOOP_ITERATIONS):
+        prior_samples.append(prior(hypercube_samples[i]))
+
+    return prior_samples
 
 
 @pytest.mark.parametrize(
@@ -39,19 +57,15 @@ def test_likelihood_dis_wmin(wmin_model_settings):
     for DIS data with wmin model.
     """
     # get chi2 with no positivity
-    loss_function = colibriAPI.make_chi2(**{**TEST_FULL_DIS_DATASET, **T0_PDFSET})
-
+    loss_function = colibriAPI.make_chi2(**{**TEST_DATASETS, **T0_PDFSET})
     # get forward map
-    forward_map = colibriAPI.make_pred_data(**TEST_FULL_DIS_DATASET)
-
+    forward_map = colibriAPI.make_pred_data(**TEST_DATASETS)
     # get FIT_XGRID
-    FIT_XGRID = colibriAPI.FIT_XGRID(**TEST_FULL_DIS_DATASET)
-
+    FIT_XGRID = colibriAPI.FIT_XGRID(**TEST_DATASETS)
     # get pdf_model
     pdf_model = wminAPI.pdf_model(
         **{**wmin_model_settings, "output_path": None, "dump_model": False}
     )
-
     # get bayesian prior
     prior = bayesian_prior(**TEST_PRIOR_SETTINGS_WMIN)
 
@@ -62,21 +76,16 @@ def test_likelihood_dis_wmin(wmin_model_settings):
         predictions, _ = pred_and_pdf(params)
         return -0.5 * loss_function(predictions)
 
-    # Sample params from the prior and compute the log likelihood
-    rng = jax.random.PRNGKey(0)
-    start_time = time.perf_counter()
-    for _ in range(N_LOOP_ITERATIONS):
-        rng, key = jax.random.split(rng)
-        hypercube = jax.random.uniform(
-            key,
-            shape=(wmin_model_settings["wmin_settings"]["n_basis"],),
-        )
-        params = prior(hypercube)
+    # Sample params from the prior
+    prior_list = prior_samples(prior, wmin_model_settings)
 
-        log_likelihood(params)
+    # evaluate likelihood time
+    start_time = time.perf_counter()
+    for i in range(N_LOOP_ITERATIONS):
+        log_likelihood(prior_list[i])
     end_time = time.perf_counter()
+
     time_per_eval = (end_time - start_time) / N_LOOP_ITERATIONS
-    print(f"Time per evaluation: {time_per_eval}")
 
     assert time_per_eval < THRESHOLD_TIME_DIS
 
@@ -91,16 +100,14 @@ def test_likelihood_dis_wmin_with_pos(wmin_model_settings):
     """
     # get chi2 with no positivity
     loss_function = colibriAPI.make_chi2_with_positivity(
-        **{**TEST_FULL_DIS_DATASET, **TEST_FULL_POS_DATASET, **T0_PDFSET}
+        **{**TEST_DATASETS, **TEST_POS_DATASET, **T0_PDFSET}
     )
 
     # get forward map
-    forward_map = colibriAPI.make_pred_data(**TEST_FULL_DIS_DATASET)
+    forward_map = colibriAPI.make_pred_data(**{**TEST_DATASETS, **TEST_POS_DATASET})
 
     # get FIT_XGRID
-    FIT_XGRID = colibriAPI.FIT_XGRID(
-        **{**TEST_FULL_DIS_DATASET, **TEST_FULL_POS_DATASET}
-    )
+    FIT_XGRID = colibriAPI.FIT_XGRID(**{**TEST_DATASETS, **TEST_POS_DATASET})
 
     # get pdf_model
     pdf_model = wminAPI.pdf_model(
@@ -117,23 +124,23 @@ def test_likelihood_dis_wmin_with_pos(wmin_model_settings):
         predictions, pdf = pred_and_pdf(params)
         return -0.5 * loss_function(predictions, pdf)
 
-    # Sample params from the prior and compute the log likelihood
-    rng = jax.random.PRNGKey(0)
-    start_time = time.perf_counter()
-    for _ in range(N_LOOP_ITERATIONS):
-        rng, key = jax.random.split(rng)
-        hypercube = jax.random.uniform(
-            key,
-            shape=(wmin_model_settings["wmin_settings"]["n_basis"],),
-        )
-        params = prior(hypercube)
+    # Sample params from the prior
+    prior_list = prior_samples(prior, wmin_model_settings)
 
-        log_likelihood(params)
+    # evaluate likelihood time
+    start_time = time.perf_counter()
+    for i in range(N_LOOP_ITERATIONS):
+        log_likelihood(prior_list[i])
     end_time = time.perf_counter()
+
     time_per_eval = (end_time - start_time) / N_LOOP_ITERATIONS
     print(f"Time per evaluation: {time_per_eval}")
 
     assert time_per_eval < THRESHOLD_TIME_DIS_POS
+
+
+if __name__ == "__main__":
+    test_likelihood_dis_wmin_with_pos(TEST_WMIN_SETTINGS_NBASIS_10)
 
 
 @pytest.mark.parametrize(
@@ -145,13 +152,13 @@ def test_likelihood_had_wmin(wmin_model_settings):
     for HAD data with wmin model.
     """
     # get chi2 with no positivity
-    loss_function = colibriAPI.make_chi2(**{**TEST_FULL_HAD_DATASET, **T0_PDFSET})
+    loss_function = colibriAPI.make_chi2(**{**TEST_DATASETS_HAD, **T0_PDFSET})
 
     # get forward map
-    forward_map = colibriAPI.make_pred_data(**TEST_FULL_HAD_DATASET)
+    forward_map = colibriAPI.make_pred_data(**TEST_DATASETS_HAD)
 
     # get FIT_XGRID
-    FIT_XGRID = colibriAPI.FIT_XGRID(**TEST_FULL_HAD_DATASET)
+    FIT_XGRID = colibriAPI.FIT_XGRID(**TEST_DATASETS_HAD)
 
     # get pdf_model
     pdf_model = wminAPI.pdf_model(
@@ -168,19 +175,15 @@ def test_likelihood_had_wmin(wmin_model_settings):
         predictions, _ = pred_and_pdf(params)
         return -0.5 * loss_function(predictions)
 
-    # Sample params from the prior and compute the log likelihood
-    rng = jax.random.PRNGKey(0)
-    start_time = time.perf_counter()
-    for _ in range(N_LOOP_ITERATIONS):
-        rng, key = jax.random.split(rng)
-        hypercube = jax.random.uniform(
-            key,
-            shape=(wmin_model_settings["wmin_settings"]["n_basis"],),
-        )
-        params = prior(hypercube)
+    # Sample params from the prior
+    prior_list = prior_samples(prior, wmin_model_settings)
 
-        log_likelihood(params)
+    # evaluate likelihood time
+    start_time = time.perf_counter()
+    for i in range(N_LOOP_ITERATIONS):
+        log_likelihood(prior_list[i])
     end_time = time.perf_counter()
+
     time_per_eval = (end_time - start_time) / N_LOOP_ITERATIONS
     print(f"Time per evaluation: {time_per_eval}")
 
@@ -197,18 +200,14 @@ def test_likelihood_had_wmin_with_pos(wmin_model_settings):
     """
     # get chi2 with no positivity
     loss_function = colibriAPI.make_chi2_with_positivity(
-        **{**TEST_FULL_HAD_DATASET, **TEST_FULL_POS_DATASET, **T0_PDFSET}
+        **{**TEST_DATASETS_HAD, **TEST_POS_DATASET, **T0_PDFSET}
     )
 
     # get forward map
-    forward_map = colibriAPI.make_pred_data(
-        **{**TEST_FULL_HAD_DATASET, **TEST_FULL_POS_DATASET}
-    )
+    forward_map = colibriAPI.make_pred_data(**{**TEST_DATASETS_HAD, **TEST_POS_DATASET})
 
     # get FIT_XGRID
-    FIT_XGRID = colibriAPI.FIT_XGRID(
-        **{**TEST_FULL_HAD_DATASET, **TEST_FULL_POS_DATASET}
-    )
+    FIT_XGRID = colibriAPI.FIT_XGRID(**{**TEST_DATASETS_HAD, **TEST_POS_DATASET})
 
     # get pdf_model
     pdf_model = wminAPI.pdf_model(
@@ -225,19 +224,15 @@ def test_likelihood_had_wmin_with_pos(wmin_model_settings):
         predictions, pdf = pred_and_pdf(params)
         return -0.5 * loss_function(predictions, pdf)
 
-    # Sample params from the prior and compute the log likelihood
-    rng = jax.random.PRNGKey(0)
-    start_time = time.perf_counter()
-    for _ in range(N_LOOP_ITERATIONS):
-        rng, key = jax.random.split(rng)
-        hypercube = jax.random.uniform(
-            key,
-            shape=(wmin_model_settings["wmin_settings"]["n_basis"],),
-        )
-        params = prior(hypercube)
+    # Sample params from the prior
+    prior_list = prior_samples(prior, wmin_model_settings)
 
-        log_likelihood(params)
+    # evaluate likelihood time
+    start_time = time.perf_counter()
+    for i in range(N_LOOP_ITERATIONS):
+        log_likelihood(prior_list[i])
     end_time = time.perf_counter()
+
     time_per_eval = (end_time - start_time) / N_LOOP_ITERATIONS
     print(f"Time per evaluation: {time_per_eval}")
 
@@ -253,13 +248,13 @@ def test_likelihood_global_wmin(wmin_model_settings):
     for global data with wmin model.
     """
     # get chi2 with no positivity
-    loss_function = colibriAPI.make_chi2(**{**TEST_FULL_GLOBAL_DATASET, **T0_PDFSET})
+    loss_function = colibriAPI.make_chi2(**{**TEST_DATASETS_DIS_HAD, **T0_PDFSET})
 
     # get forward map
-    forward_map = colibriAPI.make_pred_data(**TEST_FULL_GLOBAL_DATASET)
+    forward_map = colibriAPI.make_pred_data(**TEST_DATASETS_DIS_HAD)
 
     # get FIT_XGRID
-    FIT_XGRID = colibriAPI.FIT_XGRID(**TEST_FULL_GLOBAL_DATASET)
+    FIT_XGRID = colibriAPI.FIT_XGRID(**TEST_DATASETS_DIS_HAD)
 
     # get pdf_model
     pdf_model = wminAPI.pdf_model(
@@ -276,19 +271,15 @@ def test_likelihood_global_wmin(wmin_model_settings):
         predictions, _ = pred_and_pdf(params)
         return -0.5 * loss_function(predictions)
 
-    # Sample params from the prior and compute the log likelihood
-    rng = jax.random.PRNGKey(0)
-    start_time = time.perf_counter()
-    for _ in range(N_LOOP_ITERATIONS):
-        rng, key = jax.random.split(rng)
-        hypercube = jax.random.uniform(
-            key,
-            shape=(wmin_model_settings["wmin_settings"]["n_basis"],),
-        )
-        params = prior(hypercube)
+    # Sample params from the prior
+    prior_list = prior_samples(prior, wmin_model_settings)
 
-        log_likelihood(params)
+    # evaluate likelihood time
+    start_time = time.perf_counter()
+    for i in range(N_LOOP_ITERATIONS):
+        log_likelihood(prior_list[i])
     end_time = time.perf_counter()
+
     time_per_eval = (end_time - start_time) / N_LOOP_ITERATIONS
     print(f"Time per evaluation: {time_per_eval}")
 
@@ -305,18 +296,16 @@ def test_likelihood_global_wmin_with_pos(wmin_model_settings):
     """
     # get chi2 with no positivity
     loss_function = colibriAPI.make_chi2_with_positivity(
-        **{**TEST_FULL_GLOBAL_DATASET, **TEST_FULL_POS_DATASET, **T0_PDFSET}
+        **{**TEST_DATASETS_DIS_HAD, **TEST_POS_DATASET, **T0_PDFSET}
     )
 
     # get forward map
     forward_map = colibriAPI.make_pred_data(
-        **{**TEST_FULL_GLOBAL_DATASET, **TEST_FULL_POS_DATASET}
+        **{**TEST_DATASETS_DIS_HAD, **TEST_POS_DATASET}
     )
 
     # get FIT_XGRID
-    FIT_XGRID = colibriAPI.FIT_XGRID(
-        **{**TEST_FULL_GLOBAL_DATASET, **TEST_FULL_POS_DATASET}
-    )
+    FIT_XGRID = colibriAPI.FIT_XGRID(**{**TEST_DATASETS_DIS_HAD, **TEST_POS_DATASET})
 
     # get pdf_model
     pdf_model = wminAPI.pdf_model(
@@ -333,19 +322,15 @@ def test_likelihood_global_wmin_with_pos(wmin_model_settings):
         predictions, pdf = pred_and_pdf(params)
         return -0.5 * loss_function(predictions, pdf)
 
-    # Sample params from the prior and compute the log likelihood
-    rng = jax.random.PRNGKey(0)
-    start_time = time.perf_counter()
-    for _ in range(N_LOOP_ITERATIONS):
-        rng, key = jax.random.split(rng)
-        hypercube = jax.random.uniform(
-            key,
-            shape=(wmin_model_settings["wmin_settings"]["n_basis"],),
-        )
-        params = prior(hypercube)
+    # Sample params from the prior
+    prior_list = prior_samples(prior, wmin_model_settings)
 
-        log_likelihood(params)
+    # evaluate likelihood time
+    start_time = time.perf_counter()
+    for i in range(N_LOOP_ITERATIONS):
+        log_likelihood(prior_list[i])
     end_time = time.perf_counter()
+
     time_per_eval = (end_time - start_time) / N_LOOP_ITERATIONS
     print(f"Time per evaluation: {time_per_eval}")
 
