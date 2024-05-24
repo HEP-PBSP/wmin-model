eccc86bbf644659f945c54c9052b7f8d8cc242b1
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-05-24 14:21:32 +0100

    added regression results for MC had fit

--- colibri/models/wmin/wmin/tests/test_fit.py
+++ colibri/models/wmin/wmin/tests/test_fit.py
@@ -44,9 +44,7 @@ def test_bayesian_fits(runcard):
 
 @pytest.mark.parametrize(
     "runcard",
-    [
-        RUNCARD_TEST_FIT_WMIN_MC_DIS, RUNCARD_TEST_FIT_WMIN_MC_HAD
-    ],
+    [RUNCARD_TEST_FIT_WMIN_MC_DIS, RUNCARD_TEST_FIT_WMIN_MC_HAD],
 )
 def test_monte_carlo_fits(runcard):
     """
