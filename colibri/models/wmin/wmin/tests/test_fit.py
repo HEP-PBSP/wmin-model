123fe5ea8a6b16cc71737f01bfdd627597a269ad
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-05-24 16:25:33 +0100

    use tmp_path and -o

--- colibri/models/wmin/wmin/tests/test_fit.py
+++ colibri/models/wmin/wmin/tests/test_fit.py
@@ -18,17 +18,21 @@ from wmin.tests.wmin_conftest import (
 @pytest.mark.parametrize(
     "runcard", [RUNCARD_TEST_FIT_WMIN_BAYES_DIS, RUNCARD_TEST_FIT_WMIN_BAYES_HAD]
 )
-def test_bayesian_fits(runcard):
+def test_bayesian_fits(runcard, tmp_path):
     """
     Test the Bayesian fits using the regression runcards.
     """
-
     regression_path = "colibri/models/wmin/wmin/tests/regression_runcards"
 
-    sp.run(f"{EXE} {runcard}".split(), cwd=regression_path, check=True)
+    tmp_fit_folder = tmp_path / "tmp_bayes_fit"
+
+    # run bayesian fit at tmp_path
+    sp.run(
+        f"{EXE} {runcard} -o {tmp_fit_folder}".split(), cwd=regression_path, check=True
+    )
 
     expected_results = f"colibri/models/wmin/wmin/tests/regression_results/{runcard.split('.')[0]}/ns_result.csv"
-    actual_results = f"{regression_path}/{runcard.split('.')[0]}/ns_result.csv"
+    actual_results = f"{tmp_fit_folder}/ns_result.csv"
 
     # check that the results are as expected using the diff command
     result = sp.run(
@@ -37,36 +41,34 @@ def test_bayesian_fits(runcard):
         stderr=sp.PIPE,
     )
     assert result.returncode == 0, result.stderr
-
-    # remove the results file after the test
-    sp.run(f"rm -r {runcard.split('.')[0]}".split(), cwd=regression_path, check=True)
 
 
 @pytest.mark.parametrize(
     "runcard",
     [RUNCARD_TEST_FIT_WMIN_MC_DIS, RUNCARD_TEST_FIT_WMIN_MC_HAD],
 )
-def test_monte_carlo_fits(runcard):
+def test_monte_carlo_fits(runcard, tmp_path):
     """
     Test the Monte Carlo fits using the regression runcards.
     """
-
     regression_path = "colibri/models/wmin/wmin/tests/regression_runcards"
+    TMP_NAME = "tmp_mc_fit"
+    tmp_fit_folder = tmp_path / TMP_NAME
 
     # run monte carlo fit once
-    command = f"{EXE} {runcard} -rep 1"
+    command = f"{EXE} {runcard} -rep 1 -o {tmp_fit_folder}"
     result = sp.run(command.split(), cwd=regression_path, check=True)
 
     # run postfit
     sp.run(
-        f"{POSTFIT_EXE} {runcard.split('.')[0]} -t 1 -c 100",
+        f"{POSTFIT_EXE} {TMP_NAME} -t 1 -c 100",
         shell=True,
-        cwd=regression_path,
+        cwd=tmp_path,
         check=True,
     )
 
     expected_results = f"colibri/models/wmin/wmin/tests/regression_results/{runcard.split('.')[0]}/mc_result.csv"
-    actual_results = f"{regression_path}/{runcard.split('.')[0]}/mc_result.csv"
+    actual_results = f"{tmp_fit_folder}/mc_result.csv"
 
     # check that the results are as expected using the diff command
     result = sp.run(
@@ -75,6 +77,3 @@ def test_monte_carlo_fits(runcard):
         stderr=sp.PIPE,
     )
     assert result.returncode == 0, result.stderr
-
-    # remove the results file after the test
-    sp.run(f"rm -r {runcard.split('.')[0]}".split(), cwd=regression_path, check=True)
