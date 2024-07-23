4b5c04d6e2cf285c80ff402ef6203ba33330361a
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-07-23 11:57:49 +0100

    added overriding of ultranest_fit module in wmin

---
+++ colibri/models/wmin/wmin/ultranest_fit.py
@@ -0,0 +1,73 @@
+"""
+wmin.ultranest_fit.py
+
+This module overrides the log likelihood defined in colibri.ultranest_fit so as to allow
+the user to add model dependent terms to the likelihood
+
+"""
+
+import jax
+import jax.numpy as jnp
+from functools import partial
+from colibri.ultranest_fit import UltraNestLogLikelihood
+from colibri.loss_functions import chi2
+
+
+class WminUltraNestLogLikelihood(UltraNestLogLikelihood):
+    """
+    TODO
+    """
+
+    @partial(jax.jit, static_argnames=("self",))
+    def log_likelihood(
+        self,
+        params,
+        central_values,
+        inv_covmat,
+        fast_kernel_arrays,
+        positivity_fast_kernel_arrays,
+    ):
+        predictions, pdf = self.pred_and_pdf(params, fast_kernel_arrays)
+        return -0.5 * (
+            self.chi2(central_values, predictions, inv_covmat)
+            + jnp.sum(
+                self.penalty_posdata(
+                    pdf,
+                    self.alpha,
+                    self.lambda_positivity,
+                    positivity_fast_kernel_arrays,
+                ),
+                axis=-1,
+            )
+        + 100 * jnp.sum(params ** 2)
+        )
+
+
+def log_likelihood(
+    central_inv_covmat_index,
+    pdf_model,
+    FIT_XGRID,
+    _pred_data,
+    fast_kernel_arrays,
+    positivity_fast_kernel_arrays,
+    ns_settings,
+    _penalty_posdata,
+    alpha,
+    lambda_positivity,
+):
+    """
+    TODO
+    """
+    return WminUltraNestLogLikelihood(
+        central_inv_covmat_index,
+        pdf_model,
+        FIT_XGRID,
+        _pred_data,
+        fast_kernel_arrays,
+        positivity_fast_kernel_arrays,
+        ns_settings,
+        chi2,
+        _penalty_posdata,
+        alpha,
+        lambda_positivity,
+    )
