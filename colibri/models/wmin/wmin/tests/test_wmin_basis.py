80d1f5488875d3457432c5e174c545471c81cdf5
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-09-15 16:07:13 +0100

    adapted indices of test

--- colibri/models/wmin/wmin/tests/test_wmin_basis.py
+++ colibri/models/wmin/wmin/tests/test_wmin_basis.py
@@ -10,35 +10,44 @@ import numpy as np
 from colibri.constants import FLAVOUR_TO_ID_MAPPING
 from numpy.testing import assert_allclose
 
-@pytest.mark.parametrize(
-    "pdf_basis", ["intrinsic_charm", "perturbative_charm"]
-)
+
+@pytest.mark.parametrize("pdf_basis", ["intrinsic_charm", "perturbative_charm"])
 def test_wmin_pdfbasis_normalization(pdf_basis):
     """
     Test the normalization of the PDF basis.
     """
-    pdf_grid = np.random.rand(14, 50)
+    pdf_grid = np.random.rand(100, 14, 50)
 
-    pdf_grid_normalized = wmin_pdfbasis_normalization(pdf_grid=pdf_grid, pdf_basis=pdf_basis)
+    pdf_grid_normalized = wmin_pdfbasis_normalization(
+        pdf_grid=pdf_grid, pdf_basis=pdf_basis
+    )
 
-    sigma = pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["\Sigma"], :]
-    valence = pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V"], :]
+    sigma = pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["\Sigma"], :]
+    valence = pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V"], :]
 
     if pdf_basis == "intrinsic_charm":
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V15"],:], valence)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V24"],:], valence)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V35"],:], valence)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["T24"],:], sigma)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["T35"],:], sigma)
-    
+        assert_allclose(
+            pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V15"], :], valence
+        )
+        assert_allclose(
+            pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V24"], :], valence
+        )
+        assert_allclose(
+            pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V35"], :], valence
+        )
+        assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T24"], :], sigma)
+        assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T35"], :], sigma)
+
     elif pdf_basis == "perturbative_charm":
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V15"],:], valence)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V24"],:], valence)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["V35"],:], valence)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["T15"],:], sigma)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["T24"],:], sigma)
-        assert_allclose(pdf_grid_normalized[FLAVOUR_TO_ID_MAPPING["T35"],:], sigma)
-
-
-
-    
+        assert_allclose(
+            pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V15"], :], valence
+        )
+        assert_allclose(
+            pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V24"], :], valence
+        )
+        assert_allclose(
+            pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["V35"], :], valence
+        )
+        assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T15"], :], sigma)
+        assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T24"], :], sigma)
+        assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T35"], :], sigma)
