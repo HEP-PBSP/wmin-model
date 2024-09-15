a8531315331da5c449e47f207dda2cc68bdc2a76
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-09-15 16:54:29 +0100

    added test for wmin sum rules normaliser

--- colibri/models/wmin/wmin/tests/test_wmin_basis.py
+++ colibri/models/wmin/wmin/tests/test_wmin_basis.py
@@ -5,10 +5,20 @@ Test the wmin.basis module.
 """
 
 import pytest
-from wmin.basis import wmin_pdfbasis_normalization
 import numpy as np
-from colibri.constants import FLAVOUR_TO_ID_MAPPING
 from numpy.testing import assert_allclose
+
+from wmin.basis import (
+    wmin_pdfbasis_normalization,
+    wmin_basis_sum_rules_normalization,
+    sum_rules_dict,
+)
+from colibri.constants import FLAVOUR_TO_ID_MAPPING, LHAPDF_XGRID
+from colibri.tests.conftest import TEST_PDFSET
+
+
+from validphys.core import PDF
+from validphys import convolution
 
 
 @pytest.mark.parametrize("pdf_basis", ["intrinsic_charm", "perturbative_charm"])
@@ -51,3 +61,59 @@ def test_wmin_pdfbasis_normalization(pdf_basis):
         assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T15"], :], sigma)
         assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T24"], :], sigma)
         assert_allclose(pdf_grid_normalized[:, FLAVOUR_TO_ID_MAPPING["T35"], :], sigma)
+
+
+def test_wmin_basis_sum_rules_normalization():
+    """
+    Test the normalization of the PDF basis.
+    """
+    pdf = PDF(TEST_PDFSET)
+    sr = sum_rules_dict(pdf)[TEST_PDFSET]
+
+    pdf_grid = convolution.evolution.grid_values(
+        pdf, convolution.FK_FLAVOURS, LHAPDF_XGRID, [1.65]
+    ).squeeze(-1)
+
+    pdf_grid_sr_norm = wmin_basis_sum_rules_normalization(pdf_grid, sum_rule_dict=sr)
+
+    assert_allclose(
+        np.trapz(
+            pdf_grid_sr_norm[:, FLAVOUR_TO_ID_MAPPING["V"], :] / np.array(LHAPDF_XGRID),
+            x=LHAPDF_XGRID,
+            axis=-1,
+        ),
+        3,
+        rtol=1e-2,
+    )
+    assert_allclose(
+        np.trapz(
+            pdf_grid_sr_norm[:, FLAVOUR_TO_ID_MAPPING["V3"], :]
+            / np.array(LHAPDF_XGRID),
+            x=LHAPDF_XGRID,
+            axis=-1,
+        ),
+        1,
+        rtol=1e-2,
+    )
+    assert_allclose(
+        np.trapz(
+            pdf_grid_sr_norm[:, FLAVOUR_TO_ID_MAPPING["V8"], :]
+            / np.array(LHAPDF_XGRID),
+            x=LHAPDF_XGRID,
+            axis=-1,
+        ),
+        3,
+        rtol=1e-2,
+    )
+    assert_allclose(
+        np.trapz(
+            (
+                pdf_grid_sr_norm[:, FLAVOUR_TO_ID_MAPPING["\Sigma"], :]
+                + pdf_grid_sr_norm[:, FLAVOUR_TO_ID_MAPPING["g"], :]
+            ),
+            x=LHAPDF_XGRID,
+            axis=-1,
+        ),
+        1,
+        rtol=1e-2,
+    )
