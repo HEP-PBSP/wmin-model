350e226e7b80c3ac5101c519b0b8357cc5067668
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-10-07 09:31:58 +0100

    added module for hessian 2 mc conversion

---
+++ wmin/hessian2mc.py
@@ -0,0 +1,10 @@
+"""
+wmin.hessian2mc.py
+
+This module contains the functions that can be used to convert Hessian sets
+like MSHT20 and CT18 to Monte Carlo sets.
+The functions implemented here follow equations (4.3), for MSHT20, and (4.4), for CT18,
+of the paper arXiv:2203.05506
+"""
+
+
