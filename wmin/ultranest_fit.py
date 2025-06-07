a637fc9445a85f1037f11c3190c414a688b37dd5
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-06-07 15:37:10 +0100

    updated likelihood to reflect changes in colibri

--- wmin/ultranest_fit.py
+++ wmin/ultranest_fit.py
@@ -61,14 +61,28 @@ class WminUltraNestLogLikelihood(LogLikelihood):
     @partial(jax.jit, static_argnames=("self",))
     def log_likelihood(
         self,
-        params,
-        central_values,
-        inv_covmat,
-        fast_kernel_arrays,
-        positivity_fast_kernel_arrays,
-    ):
+        params: jnp.array,
+        central_values: jnp.array,
+        inv_covmat: jnp.array,
+        fast_kernel_arrays: tuple,
+        positivity_fast_kernel_arrays: tuple,
+    ) -> jnp.array:
         """
-        Computes the log likelihood for the wmin model.
+        This function takes care of computing the log_likelihood that is defined in LogLikelihood.
+        Function is jax.jit compiled for better performance.
+
+        Parameters
+        ----------
+        params: jnp.array
+        central_values: jnp.array
+        inv_covmat: jnp.array
+        fast_kernel_arrays: tuple
+        positivity_fast_kernel_arrays: tuple
+
+        Returns
+        -------
+        jnp.array
+            jax array with the value of the log-likelihood.
         """
         predictions, pdf = self.pred_and_pdf(params, fast_kernel_arrays)
 
@@ -79,17 +93,30 @@ class WminUltraNestLogLikelihood(LogLikelihood):
         else:
             regularisation_term = 0.0
 
-        return -0.5 * (
-            self.chi2(central_values, predictions, inv_covmat)
-            + jnp.sum(
+        if self.positivity_penalty_settings["positivity_penalty"]:
+            pos_penalty = jnp.sum(
                 self.penalty_posdata(
                     pdf,
-                    self.alpha,
-                    self.lambda_positivity,
+                    self.positivity_penalty_settings["alpha"],
+                    self.positivity_penalty_settings["lambda_positivity"],
                     positivity_fast_kernel_arrays,
                 ),
                 axis=-1,
             )
+        else:
+            pos_penalty = 0
+
+        integ_penalty = jnp.sum(
+            self.integrability_penalty(
+                pdf,
+            ),
+            axis=-1,
+        )
+
+        return -0.5 * (
+            self.chi2(central_values, predictions, inv_covmat)
+            + pos_penalty
+            + integ_penalty
             + regularisation_term
         )
 
